c23ca12084a8ee9b3e41e6d21e9fbad7b7a665a2
Hoggaan <mahuraanhoggaan@gmail.com>
2023-07-05 07:38:07 +0530

    second-day

--- numpy_hacker_rank_1.py
+++ numpy_hacker_rank_1.py
@@ -35,5 +35,32 @@ import random
 lst = [4,3,5]
 for i in range(6,9):
     lst = lst + [i]
-print(lst)
+# print(lst)
 
+arr = np.array([1,2,3])
+arr1 = 2**arr
+arr2 = arr ** 2
+# print(arr1, arr2)
+arr = np.array(range(10))
+lst = list(range(10))
+# print(lst)
+
+# arr1 = np.array(range(0,10,0.5))
+arr2 = np.arange(0,10, 0.5)
+# print(arr1)
+# print(arr2)
+
+'''---------------------------------------'''
+arr = np.array([1, "kow"])
+# print(arr.dtype)
+# print(np.array([1,2,3]).dtype)
+# print(np.array([0.2, 1]).dtype)
+
+# In numpy the array elements must be the same data type. 
+# If you give different elements with different data types it will convert
+# The same data type. 
+
+""" Changing the data type of an array"""
+# arr2 = arr2.astype("int")
+arr2 = arr2.astype("float")
+print(arr2)

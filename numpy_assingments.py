a7062102d1b006b9cd3d24dbf9d1a4b4fde52816
Hoggaan <mahuraanhoggaan@gmail.com>
2023-07-11 10:42:08 +0530

    Updated

--- numpy_assingments.py
+++ numpy_assingments.py
@@ -4,7 +4,7 @@ import numpy as np
 a = np.array([[6, 28], [8, 56], [7, 19]])
 
 x=np.transpose(a).reshape(1,6)
-print(x.shape)
+# print(x.shape)
 
 # Take away: (1,6) = [[]]. 2D array
 
@@ -13,9 +13,31 @@ a = np.array([[16, 5], [81, 6], [33, 1]])
 
 x=np.transpose(a).reshape(2,3)
 
-print(x.flatten().shape)
+# print(x.flatten().shape)
 # Take away: flatten() gives you []. 1 D array
 
 a = np.array([[34, 28,55], [8, 56, 3], [77, 87, 19]])
 
-print(a.transpose()[-2,-2])
+# print(a.transpose()[-2,-2])
+
+# -----------
+
+# arr = 2*np.arange(0,2,0.5)
+# if np.any(arr <= 0.6):
+#     print ("condition satisfies")
+# else:
+#     print ("condition doesn't satisfy")
+
+
+arr = np.array([1,2,3,0,-2,4])
+arr1 = arr1 = arr[arr > 0]
+arr[0] = 10
+# print(arr)
+# print(arr1)
+
+arr = np.arange(16).reshape(4,4)  
+
+print(np.split(arr, 4))
+ 
+
+
--- numpy_memorizing.py
+++ numpy_memorizing.py
@@ -5,5 +5,29 @@ arr = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 arr1 = np.reshape(arr, (4,3))
 
 """Indexing"""
-print(arr[::2])
-print(arr1[:,::2])
+
+# print(arr[:])
+# print(arr1[:,::2])
+
+# print(np.array([12,3,4], int))
+
+## Sorting - np.argsort() and np.sort()
+
+arr = np.array([3,5,2])
+# print(np.sort(arr))
+# arr.sort()
+# print(arr) # This returns None. 
+
+arr = np.array([5,3,26,732,29])
+arr.sort()
+# print(arr)
+
+lst = [5,3,26,732,29] 
+# print(lst.sort()) # --> None
+lst.sort() # Original gets sorted. 
+# print(lst)
+
+arr = np.array([[5,3,2,8], [5,2,6,4]])
+print(np.sort(arr,1)) # --> [[2,3,5,8], [2,4,5,6]]
+arr.sort()
+print(arr) # --> [[2,3,5,8], [2,4,5,6]]
